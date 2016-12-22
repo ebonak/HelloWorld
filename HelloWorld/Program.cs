970e1e4fe8f4dca9e708f64a9df9d3babb2acab7
Esmail Bonakdarian <ebonakdarian@dom.edu>
2016-12-22 07:01:57 -0600

    Added a 3rd line and greeting :)

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -12,6 +12,7 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("Hello GitHub!");
+            Console.WriteLine("Whaazup peeps!!!!?");
         }
     }
 }
